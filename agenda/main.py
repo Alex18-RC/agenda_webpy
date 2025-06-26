75d7745598eb472279f9531b8a95881c68bca4a4
Salvador HM <salvadorhm@gmail.com>
2025-06-26 08:39:20 -0600

    CREATED detalle v1

--- agenda/main.py
+++ agenda/main.py
@@ -4,6 +4,7 @@ import sqlite3
 urls = (
     "/", "Index",
     "/insertar","Insertar",
+    "/detalle/(.*)", "Detalle"
     )
 
 render = web.template.render("templates/")
@@ -20,7 +21,6 @@ class Index:
                 "personas" : personas.fetchall(),
                 "error": None
             }
-            print(f"RESPUESTA: {respuesta}")
             return render.index(respuesta)
         except sqlite3.OperationalError as error:
             print(f"Error 000: {error.args[0]}")
@@ -58,6 +58,30 @@ class Insertar:
         except Exception as error:
             print(f"Error 003: {error.args[0]}")
             return web.seeother("/")
+
+
+class Detalle:
+
+    def GET(self,id_persona):
+        try:
+            conection = sqlite3.connect("agenda.db")
+            cursor = conection.cursor()
+            sql = "select * from personas where id_persona = ?;"
+            datos = (id_persona,)
+            personas = cursor.execute(sql,datos)
+            
+            respuesta={
+                "persona" : personas.fetchone(),
+                "error": None
+            }
+            print(f"RESPUESTA: {respuesta}")
+            return render.detalle(respuesta)
+        except sqlite3.OperationalError as error:
+            print(f"Error 004: {error.args[0]}")
+            respuesta={
+                "persona" : {},
+                "error": "Error en la base de datos"
+            return render.detalle(respuesta)
 
 application = app.wsgifunc()
 
